5e3692f088a17fc8079e171e0499d99992132f11
nich <nicobaccy@icloud.com>
2024-05-02 19:45:05 +0200

    Add request class

---
+++ src/Carbo.Core/Models/Http/Request.cs
@@ -0,0 +1,14 @@
+﻿namespace Carbo.Core.Models.Http
+{
+    /// <summary>
+    /// Class that represents a request to be made by the client.
+    /// </summary>
+    public class Request
+    {
+        public HttpMethod HttpMethod { get; set; }
+        public Uri Url { get; set; }
+        public List<RequestKeyValueParameter> QueryParameters { get; set; }
+        public List<RequestKeyValueParameter> Headers { get; set; }
+        public HttpContent Content { get; set; }
+    }
+}
