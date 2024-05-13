02827a04742c4e7f458c437121e2f1088e412071
nich <nicobaccy@icloud.com>
2024-05-13 18:54:47 +0200

    Add merged resource dictionary

---
+++ src/Carbo/Views/QuickRequestView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Carbo;
+
+public partial class QuickRequestView : UserControl
+{
+    public QuickRequestView()
+    {
+        InitializeComponent();
+    }
+}
